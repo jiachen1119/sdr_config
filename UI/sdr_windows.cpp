edfee9266df564365edb92225f974d6a6cc5a132
tang <220223251@seu.edu.cn>
2022-12-30 12:29:04 +0800

    sdr config first

--- UI/sdr_windows.cpp
+++ UI/sdr_windows.cpp
@@ -64,11 +64,40 @@ sdr_windows::sdr_windows(QWidget *parent) :
     connect(ui->pushButton_2,&QPushButton::clicked, this,[&](){
         QString dataType=ui->comboBox->currentText();
         QString samplingFreq=ui->lineEdit->text();
+        QFile openFile("../1.conf");
+        QStringList readQStringList;
+        if(!openFile.open(QIODevice::ReadOnly|QIODevice::Text|QIODevice::ExistingOnly))
+            qDebug()<<"cannot find the model configuration file";
+        else{
+            char* readLineData=new char[100];
+            qint64 readNum=openFile.readLine(readLineData,100);
+            while ((readNum !=0) && (readNum != -1)){
+                QString dataQString(readLineData);
+                QString confKeyWord=dataQString.split("=").at(0);
+                if(confKeyWord.contains("sampling_frequency", Qt::CaseInsensitive)){
+                    QStringList dataList=dataQString.split("=");
+                    QString str=QString("%1=%2\n").arg(confKeyWord).arg(samplingFreq);
+                    dataQString=str;
+                }
+                if(dataQString.contains("item_type", Qt::CaseInsensitive)){
+                    QStringList dataList=dataQString.split("=");
+                    QString str=QString("%1=%2\n").arg(confKeyWord).arg(dataType);
+                    dataQString=str;
+                }
+                readQStringList<<dataQString;
+                readNum=openFile.readLine(readLineData, 100);
+            }
+            openFile.close();
+        }
         QFile writeFile("../sdr_config.conf");
         if(!writeFile.open(QIODevice::WriteOnly|QIODevice::Text))
             qDebug()<<"sdr_config.conf don't exist";
-        writeFile.write(samplingFreq.toStdString().c_str());
-        writeFile.close();
+        else{
+            for (int index = 0; index < readQStringList.size(); ++index) {
+                writeFile.write(readQStringList.at(index).toStdString().c_str());
+            }
+            writeFile.close();
+        }
     });
 
     //set the Default button
